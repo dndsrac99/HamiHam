dacbeb2333d01c5aad25ca1c5044f563fcee2b95
Marton Tolnai <marton.tolnai@atp-autoteile.de>
2020-10-06 12:22:58 +0200

    7: Washington.vue

---
+++ src/components/Washington.vue
@@ -0,0 +1,49 @@
+<template>
+<v-card shaped height="300">
+    <v-app-bar color="red leighten-1" height="80" >
+        <v-avatar size="72" class="mr-2">
+            <img src="https://i.pinimg.com/originals/c4/79/fc/c479fc768b081b6e50ae4773667e47f2.jpg" alt="">
+        </v-avatar>
+        <v-card-title class="whiteText">Washingmachine</v-card-title>
+
+    </v-app-bar>
+    <v-card-title><v-icon>mdi-human-male-male</v-icon> Army:{{soldiers}} </v-card-title>
+            <v-row>
+            <v-btn class="leftText white--text" rounded color="blue darken-2"> Hire</v-btn>
+        </v-row>
+</v-card>
+</template>
+<script>
+export default {
+    data(){
+        return{
+                soldiers: this.$money.soldiers
+            }
+        
+    },
+    watch:{
+        $money(){
+            this.soldiers = this.$money.soldiers;
+        }
+    },
+    functions:{
+
+    },
+}
+</script>
+<style>
+.whiteText{
+    color: white;
+}
+.leftText {
+  margin-left: 30px;
+  margin-top: 1%;
+  margin-bottom: 1%;
+  color: white;
+}
+.rightText {
+  margin-right: 30px;
+  margin-top: 1%;
+  margin-bottom: 1%;
+}
+</style>
